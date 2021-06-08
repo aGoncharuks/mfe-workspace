60a3f0233a8a024ef6d1596050f25f6dbfc93679
Aleksandrs Goncaruks <aleksandrs.goncaruks@28stone.com>
2021-06-08 22:00:12 +0300

    feat: add simple react hook to appOne

--- packages/app-one/src/App.js
+++ packages/app-one/src/App.js
@@ -1,7 +1,13 @@
+import {useState} from 'react';
+
 function App() {
+	const [count, setCount] = useState(0);
+	
   return (
     <div className="App">
-			React micro frontend here
+			<div>REACT APPLICATION</div>
+			<div>Counter: {count}</div>
+			<button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
   );
 }
